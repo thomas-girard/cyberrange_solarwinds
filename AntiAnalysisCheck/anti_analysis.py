90b1e3f6b3259fbd0dd2e1c0153a582012d2c4f2
Thomas Girard <70712826+thomas-girard@users.noreply.github.com>
2022-02-04 14:59:40 +0100

    detection service + kill if it's in the list

--- AntiAnalysisCheck/anti_analysis.py
+++ AntiAnalysisCheck/anti_analysis.py
@@ -12,11 +12,22 @@ def create_dico_process():
     dico_process =  {}
     for proc in psutil.process_iter():
         try:
-            dico_process[proc.name().split("/")[0]] = proc.pid
+            dico_process[proc.name().split(".")[0].lower()] = proc.pid # it's necessary to split in order to remove potentially ".exe"
 
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     return dico_process
+
+def create_dico_service():
+    """
+    Get all service name and return a dictionary
+    """
+
+    list_service = list(psutil.win_service_iter())
+    dico_service = {}
+    for name_instance in range(len(list_service)):
+        dico_service[list_service[name_instance].name().lower()] = psutil.win_service_get(list_service[name_instance].name()).as_dict()
+    return dico_service
 
 
 def calculate_hash(string):
@@ -34,23 +45,53 @@ def get_list_hash(name):
 
     with open(name, 'r') as hash_doc:
         list = hash_doc.readlines()
-    return list
+    list_int = []
+    for k in range(len(list)):
+        list_int.append(int(list[k]))
+    return list_int
 
 
 if __name__ == "__main__":
+
     dico_process = create_dico_process()
-    list_hash = get_list_hash("hash.txt")
 
-    for process in [*dico_process]:
-        hash_process = calculate_hash(process)
-        if hash_process in list_hash:
-            print("Detection of a process in the list : ", process)
+    dico_service = create_dico_service()
 
+    try:
+        list_hash = get_list_hash("hash.txt")
+        detection_bool_process = False
+        for process in [*dico_process]:
+            hash_process = calculate_hash(process)
+            if hash_process in list_hash:
+                print("Detection of a process in the list : ", process)
+                detection_bool_process = True
 
-    print("there is no running process in the hash list !")
-    #print(calculate_hash('autopsy'))
+        if detection_bool_process == False:
+            print("No process is in the hash list !")
+    except:
+        pass
 
-    #print(list_hash)
+    try:
+        list_hash = get_list_hash("hash.txt")
+        detection_bool_service = False
+        for service in [*dico_service]:
+            hash_service = calculate_hash(process)
+            if hash_service in list_hash:
+                print("Detection of a service in the list : ", process)
+                detection_bool_service = True
 
-    #print(dico_process)
+                if dico_service[service]['pid'] is not None:
+                    print("attempt to kill the service...")
+                    try:
+                        os.kill(dico_service[service]['pid'], 9)
+                        print("Service killed ! ")
+                    except:
+                        print("Failure to stop service")
+
+        if detection_bool_service == False:
+            print("No service is in the hash list !")
+    except:
+        pass
+    
+
     input("Press enter to exit...")
