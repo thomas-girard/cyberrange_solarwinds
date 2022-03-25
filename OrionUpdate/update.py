526e9a8c2b433031705ec17d328f77aa3f1b0b00
Tanguy Boisset <tanguy.boisset@gmail.com>
2022-03-25 12:46:47 +0100

    added prints

--- OrionUpdate/update.py
+++ OrionUpdate/update.py
@@ -5,6 +5,9 @@ import requests
 configName = 'updateOrion.conf'
 
 root_dir = "C:/Users/"
+
+print("Fetching configuration file " + configName + "...")
+
 for filename in glob.iglob(root_dir + '**/**', recursive=True):
     if configName in filename:
         path = filename
@@ -15,22 +18,28 @@ except:
     print("Error : No config file " + configName + " found or the file is not readable/writable\n")
     sys.exit(1)
 
+print("File found !\n")
+print("Parsing configuration file...\n")
+
 for line in file.readlines():
     if 'solarwindsIP' in line:
         IPOrion = line[15:-1]
-        pass
+        continue
     if 'path' in line:
         path = line[7:-1]
-        pass
+        continue
     if 'url' in line:
         url = line[6:-1]
-        pass
+        continue
     if 'filename' in line:
         fname = line[11:-1]
-        pass
+        continue
 
-url = url
+print("Downloading Orion update...\n")
+
 r = requests.get("http://" + IPOrion + url)
 open(path + fname , 'wb').write(r.content)
+
+print("Update successfully downloaded at : " + path + fname)
 
 sys.exit(0)
